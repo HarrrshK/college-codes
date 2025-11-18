b4f2707b4e74b300dd495561fb7db9cc7fa732e1
TheSnitcher <helloharsh24@gmail.com>
2025-11-18 11:12:34 +0530

    exp2

---
+++ assignments/exp2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Sum = %d\n", a + b);
+    return 0;
+}
+
+
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Difference = %d\n", a - b);
+    return 0;
+}
+
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Product = %d\n", a * b);
+    return 0;
+}
+
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Quotient = %d\n", a / b);
+    return 0;
+}
+
+
+
+// comment out the rest except for the one you wanna use
